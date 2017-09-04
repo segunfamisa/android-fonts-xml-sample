d80ca9f092e61b1689361c49103cdd1eacda863b
Segun Famisa <segunfamisa@gmail.com>
2017-09-04 18:24:42 +0100

    [chore] Add font code samples

--- app/src/main/java/com/segunfamisa/sample/xmlfonts/MainActivity.java
+++ app/src/main/java/com/segunfamisa/sample/xmlfonts/MainActivity.java
@@ -1,13 +1,21 @@
 package com.segunfamisa.sample.xmlfonts;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView fontText = findViewById(R.id.text_programmatic);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.app_font);
+        fontText.setTypeface(typeface);
     }
 }
